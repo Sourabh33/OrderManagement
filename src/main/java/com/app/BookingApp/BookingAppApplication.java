11e2c049d353d878faddaba0cead97fc5d267b65
Sourabh Shrivastava <sourabhshrivastava33@gmail.com>
2021-11-05 16:18:20 +0530

    Removed Comments

--- src/main/java/com/app/BookingApp/BookingAppApplication.java
+++ src/main/java/com/app/BookingApp/BookingAppApplication.java
@@ -4,18 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.util.UriComponentsBuilder;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class BookingAppApplication {
 
 	public static void main(String[] args) {
-		String string = UriComponentsBuilder.fromUriString("TEST")
-				.queryParam("user", "username")
-				.queryParam("password", "password")
-				.queryParam("port", 22)
-				.queryParam("host", "host")
-				.build().toString();
-		System.out.println(string);
-//		SpringApplication.run(BookingAppApplication.class, args);
+		SpringApplication.run(BookingAppApplication.class, args);
 	}
 
 }
