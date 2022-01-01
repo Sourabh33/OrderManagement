b889cfdbccc1d6e14f5a18358180000a4fc11684
Sourabh Shrivastava <sourabhshrivastava33@gmail.com>
2022-01-01 19:42:34 +0530

    updated cross origin

--- src/main/java/com/app/orderapp/carts/CartController.java
+++ src/main/java/com/app/orderapp/carts/CartController.java
@@ -2,6 +2,7 @@ package com.app.orderapp.carts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("cart")
+@CrossOrigin
 public class CartController {
 
     @Autowired
